84711adba09c64b75369f8b801819a90198238c6
Ivan Topolnak <ivantopo@gmail.com>
2013-11-04 18:11:16 -0300

    wip

--- kamon-core/src/main/scala/kamon/Kamon.scala
+++ kamon-core/src/main/scala/kamon/Kamon.scala
@@ -20,53 +20,6 @@ object Kamon {
   def apply[T <: Extension](key: ExtensionId[T])(implicit system: ActorSystem): ActorRef = key(system).manager
 
 
-
-
-
-
-
-
   implicit lazy val actorSystem = ActorSystem("kamon")
-
-  object Metric {
-
-    val actorSystems =  TrieMap.empty[String, ActorSystemMetrics]
-
-    def actorSystemNames: List[String] = actorSystems.keys.toList
-    def registerActorSystem(name: String) = actorSystems.getOrElseUpdate(name, ActorSystemMetrics(name))
-
-    def actorSystem(name: String): Option[ActorSystemMetrics] = actorSystems.get(name)
-  }
-
-  //val metricManager = actorSystem.actorOf(Props[MetricManager], "metric-manager")
-  //val newrelicReporter = actorSystem.actorOf(Props[NewrelicReporterActor], "newrelic-reporter")
-
 }
 
-
-class MetricManager extends Actor {
-  implicit val ec = context.system.dispatcher
-
-  def receive = {
-    case RegisterForAllDispatchers(frequency) => {
-      val subscriber = sender
-      context.system.scheduler.schedule(frequency, frequency) {
-        Kamon.Metric.actorSystems.foreach {
-          case (asName, actorSystemMetrics) => actorSystemMetrics.dispatchers.foreach {
-            case (dispatcherName, dispatcherMetrics) => {
-              val activeThreads = dispatcherMetrics.activeThreadCount.snapshot
-              val poolSize = dispatcherMetrics.poolSize.snapshot
-              val queueSize = dispatcherMetrics.queueSize.snapshot
-
-              subscriber ! DispatcherMetrics(asName, dispatcherName, activeThreads, poolSize, queueSize)
-
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-case class RegisterForAllDispatchers(frequency: FiniteDuration)
-case class DispatcherMetrics(actorSystem: String, dispatcher: String, activeThreads: HistogramSnapshot, poolSize: HistogramSnapshot, queueSize: HistogramSnapshot)
