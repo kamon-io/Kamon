2c8f103b29bb69c27bf5e00b50a7391ae8cc687e
Ivan Topolnak <ivantopo@gmail.com>
2013-11-11 16:10:57 -0300

    Make sure NewRelicErrorLogger manages all cases

--- project/Settings.scala
+++ project/Settings.scala
@@ -3,7 +3,7 @@ import Keys._
 import spray.revolver.RevolverPlugin.Revolver
 
 object Settings {
-  val VERSION = "0.0.6"
+  val VERSION = "0.0.9"
 
   lazy val basicSettings = seq(
     version       := VERSION,
