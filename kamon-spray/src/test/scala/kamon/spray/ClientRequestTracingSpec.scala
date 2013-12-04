5e214a33acabe972865dfab637556d680925d869
Ivan Topolnjak <ivantopo@gmail.com>
2013-12-04 09:24:53 -0300

    first approach to client instrumentation testing

---
+++ kamon-spray/src/test/scala/kamon/spray/ClientRequestTracingSpec.scala
@@ -0,0 +1,31 @@
+package kamon.spray
+
+import akka.testkit.TestKit
+import akka.actor.ActorSystem
+import org.scalatest.WordSpecLike
+import spray.httpx.RequestBuilding
+import spray.client.pipelining._
+import kamon.trace.{UowTrace, Trace}
+import scala.concurrent.Await
+
+class ClientRequestTracingSpec extends TestKit(ActorSystem("server-request-tracing-spec")) with WordSpecLike with RequestBuilding with TestServer {
+  implicit val ec = system.dispatcher
+
+
+  "the client instrumentation" should {
+    "record segments for a client http request" in {
+
+      Trace.start("record-segments")(system)
+
+      send {
+        Get(s"http://127.0.0.1:$port/ok")
+
+      // We don't care about the response, just make sure we finish the Trace after the response has been received.
+      } map(rsp => Trace.finish())
+
+      val trace = expectMsgType[UowTrace]
+      println(trace.segments)
+    }
+  }
+
+}
