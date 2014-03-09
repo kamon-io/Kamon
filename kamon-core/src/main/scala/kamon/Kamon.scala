60f8dd3be4593cb55e5c4023b7041ad94f6b8c8a
Diego <diegolparra@gmail.com>
2014-03-09 15:01:51 -0300

    Play Integration with: X-Trace-Token, WebExternal Time and Error publishing in Akka EventStream

--- kamon-core/src/main/scala/kamon/Kamon.scala
+++ kamon-core/src/main/scala/kamon/Kamon.scala
@@ -16,12 +16,15 @@
 package kamon
 
 import akka.actor._
-import akka.event.Logging.Info
+import akka.event.Logging.{ Info, Error }
 
 object Kamon {
   trait Extension extends akka.actor.Extension {
     def publishInfoMessage(system: ActorSystem, msg: String): Unit = {
       system.eventStream.publish(Info("", classOf[Extension], msg))
+    }
+    def publishErrorMessage(system: ActorSystem, msg: String, cause: Throwable): Unit = {
+      system.eventStream.publish(new Error(cause, "", classOf[Extension], msg))
     }
   }
 
