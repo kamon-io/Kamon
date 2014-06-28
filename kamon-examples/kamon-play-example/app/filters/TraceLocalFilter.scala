baab5644a65476e9c812a002d3c2b2b715619d11
Diego <diegolparra@gmail.com>
2014-06-28 19:41:29 -0300

    = kamon-examples: fixes typo in TraceLocalFilter

--- kamon-examples/kamon-play-example/app/filters/TraceLocalFilter.scala
+++ kamon-examples/kamon-play-example/app/filters/TraceLocalFilter.scala
@@ -1,3 +1,19 @@
+/* ===================================================
+ * Copyright © 2013-2014 the kamon project <http://kamon.io/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
+
 package filters
 
 import kamon.trace.{TraceRecorder, TraceLocal}
@@ -15,7 +31,7 @@ object TraceLocalKey extends TraceLocal.TraceLocalKey {
  extra headers, with kamon it's possible using TraceLocalStorage, in Play applications we can do an Action Filter or using Action Composition,
  in this example we are using a simple filter where given a Header store the value and then put the value in the result headers..
 
- More detailed used of TraceLocalStorage: https://github.com/kamon-io/Kamon/blob/b17539d231da923ea854c01d2c69eb02ef1e85b1/kamon-core/src/test/scala/kamon/trace/TraceLocalSpec.scala
+ More detailed usage of TraceLocalStorage: https://github.com/kamon-io/Kamon/blob/b17539d231da923ea854c01d2c69eb02ef1e85b1/kamon-core/src/test/scala/kamon/trace/TraceLocalSpec.scala
  */
 object TraceLocalFilter extends Filter {
   val logger = Logger(this.getClass)
