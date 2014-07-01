8bf93c1f26453ef298d965aaa8b9cb932f8b2a64
Diego <diegolparra@gmail.com>
2014-07-01 17:18:42 -0300

    = kamon-play-example: fixed typo

--- kamon-examples/kamon-play-example/app/controllers/KamonPlayExample.scala
+++ kamon-examples/kamon-play-example/app/controllers/KamonPlayExample.scala
@@ -22,34 +22,30 @@ import play.api.mvc.{Action, Controller}
 
 import scala.concurrent._
 
-
 /**
-In order to run the example we need set the -agent parameter to the JVM but Play have some limitations when trying setear an
-java agent in Play dev mode (ie, play run) -> https://github.com/playframework/playframework/issues/1372, so we have others options:
-
-
-The first option is set -javaagent: path-to-aspectj-weaver in your IDE or
-
-Run the following commands from console:
-
-1- play stage
-2- cd target/universal/stage
-3- java -cp ".:lib/*" -javaagent:lib/org.aspectj.aspectjweaver-1.8.1.jar play.core.server.NettyServer
-
-and finally for test:
-
-curl -i -H 'X-Trace-Token:kamon-test' -H 'MyTraceLocalStorageKey:extra-header' -X GET "http://localhost:9000/helloKamon"
-
-we should get:
-
-HTTP/1.1 200 OK
-Content-Type: text/plain; charset=utf-8
-MyTraceLocalStorageKey: extra-header -> Extra Information
-X-Trace-Token: kamon-test -> default Trace-Token
-Content-Length: 18
-
-Say hello to Kamon
-  **/ **/
+ * In order to run the example we need set the -agent parameter to the JVM but Play have some limitations when trying to set an
+ * java agent in Play dev mode (ie, play run) -> https://github.com/playframework/playframework/issues/1372, so we have others options:
+ *
+ * The first option is set -javaagent: path-to-aspectj-weaver in your IDE or
+ *
+ * Run the following commands from console:
+ *
+ * 1- play stage
+ * 2- cd target/universal/stage
+ * 3- java -cp ".:lib/*" -javaagent:lib/org.aspectj.aspectjweaver-1.8.1.jar play.core.server.NettyServer
+ *
+ * and finally for test:
+ *
+ * curl -i -H 'X-Trace-Token:kamon-test' -H 'MyTraceLocalStorageKey:extra-header' -X GET "http://localhost:9000/helloKamon"
+ *
+ * we should get:
+ * HTTP/1.1 200 OK
+ * Content-Type: text/plain; charset=utf-8
+ * MyTraceLocalStorageKey: extra-header -> Extra Information
+ * X-Trace-Token: kamon-test -> default Trace-Token
+ *
+ * Say hello to Kamon
+ */*/
 
 
 object KamonPlayExample extends Controller {
