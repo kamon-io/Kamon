0bb57412690b8694f742d479213bda6f7f090630
Ivan Topolnjak <ivantopo@gmail.com>
2014-11-09 20:44:56 +0100

    ! build: only depend on aspectjweaver with provided scope, closes #106

--- project/AspectJ.scala
+++ project/AspectJ.scala
@@ -1,17 +1,26 @@
 import sbt._
 import sbt.Keys._
-import com.typesafe.sbt.SbtAspectj.{ Aspectj, aspectjSettings }
+import com.typesafe.sbt.SbtAspectj.{ Aspectj, defaultAspectjSettings }
 import com.typesafe.sbt.SbtAspectj.AspectjKeys.{ aspectjVersion, compileOnly, lintProperties, weaverOptions }
 
 
 object AspectJ {
 
-  lazy val aspectJSettings = aspectjSettings ++ Seq(
+  lazy val aspectJSettings = inConfig(Aspectj)(defaultAspectjSettings) ++ aspectjDependencySettings ++ Seq(
    aspectjVersion in Aspectj    :=  Dependencies.aspectjVersion,
       compileOnly in Aspectj    :=  true,
                 fork in Test    :=  true,
          javaOptions in Test  <++=  weaverOptions in Aspectj,
           javaOptions in run  <++=  weaverOptions in Aspectj,
    lintProperties in Aspectj    +=  "invalidAbsoluteTypeName = ignore"
+  )
+
+  def aspectjDependencySettings = Seq(
+    ivyConfigurations += Aspectj,
+    libraryDependencies <++= (aspectjVersion in Aspectj) { version => Seq(
+      "org.aspectj" % "aspectjtools" % version % Aspectj.name,
+      "org.aspectj" % "aspectjweaver" % version % Aspectj.name,
+      "org.aspectj" % "aspectjrt" % version % Aspectj.name
+    )}
   )
 }
