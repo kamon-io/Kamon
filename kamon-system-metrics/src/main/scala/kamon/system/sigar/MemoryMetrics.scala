9bb16ef93d43c5115e492bcb779d4524a2556983
Ivan Topolnjak <ivantopo@gmail.com>
2015-01-26 19:51:44 +0100

    + sysmetrics: report total and buffers+cache system memory

--- kamon-system-metrics/src/main/scala/kamon/system/sigar/MemoryMetrics.scala
+++ kamon-system-metrics/src/main/scala/kamon/system/sigar/MemoryMetrics.scala
@@ -13,16 +13,21 @@ import org.hyperic.sigar.Sigar
  */
 class MemoryMetrics(instrumentFactory: InstrumentFactory) extends GenericEntityRecorder(instrumentFactory) with SigarMetric {
   val used = histogram("memory-used", Memory.Bytes)
+  val cached = histogram("memory-cache-and-buffer", Memory.Bytes)
   val free = histogram("memory-free", Memory.Bytes)
+  val total = histogram("memory-total", Memory.Bytes)
   val swapUsed = histogram("swap-used", Memory.Bytes)
   val swapFree = histogram("swap-free", Memory.Bytes)
 
   def update(sigar: Sigar): Unit = {
     val mem = sigar.getMem
     val swap = sigar.getSwap
+    val cachedMemory = if(mem.getActualFree > mem.getFree) mem.getActualFree - mem.getFree else 0L
 
-    used.record(mem.getUsed)
-    free.record(mem.getFree)
+    used.record(mem.getActualUsed)
+    free.record(mem.getActualFree)
+    cached.record(cachedMemory)
+    total.record(mem.getTotal)
     swapUsed.record(swap.getUsed)
     swapFree.record(swap.getFree)
   }
