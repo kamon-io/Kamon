4a918ce6570129d3d4fd2d3e98d43f8078bb2d99
Eugene Platonov <jozic@live.com>
2015-12-01 13:26:50 -0500

    + core: provide generic way to scale time and memory metrics

--- kamon-core/src/test/scala/kamon/metric/instrument/HistogramSpec.scala
+++ kamon-core/src/test/scala/kamon/metric/instrument/HistogramSpec.scala
@@ -48,7 +48,7 @@ class HistogramSpec extends WordSpec with Matchers {
     }
 
     "produce a snapshot" which {
-      "supports min, max, percentile, sum and numberOfMeasurements operations" in new HistogramFixture {
+      "supports min, max, percentile, sum, numberOfMeasurements and recordsIterator operations" in new HistogramFixture {
         histogram.record(100)
         histogram.record(200, count = 200)
         histogram.record(300)
@@ -64,6 +64,36 @@ class HistogramSpec extends WordSpec with Matchers {
         snapshot.sum should be(41300)
         snapshot.numberOfMeasurements should be(203)
 
+        val records = snapshot.recordsIterator.map(r => r.level -> r.count).toSeq
+        records.size should be (4)
+        records(0) should be(100 -> 1)
+        records(1) should be(200 -> 200)
+        records(2) should be(300 -> 1)
+        records(3) should be(900 -> 1)
+      }
+
+      "can be scaled" in new HistogramFixture {
+        histogram.record(100)
+        histogram.record(200, count = 200)
+        histogram.record(300)
+        histogram.record(900)
+
+        val snapshot = takeSnapshot().scale(Time.Seconds, Time.Milliseconds)
+
+        snapshot.min should equal(100000L +- 1000L)
+        snapshot.max should equal(900000L +- 9000L)
+        snapshot.percentile(50.0D) should be(200000)
+        snapshot.percentile(99.5D) should be(300000)
+        snapshot.percentile(99.9D) should be(900000)
+        snapshot.sum should be(41300000)
+        snapshot.numberOfMeasurements should be(203)
+
+        val records = snapshot.recordsIterator.map(r => r.level -> r.count).toSeq
+        records.size should be (4)
+        records(0) should be(100000 -> 1)
+        records(1) should be(200000 -> 200)
+        records(2) should be(300000 -> 1)
+        records(3) should be(900000 -> 1)
       }
 
       "can be merged with another snapshot" in new MultipleHistogramFixture {
