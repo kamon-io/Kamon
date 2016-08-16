2006cc05948a53a45cffab70ea71d97f600bf2c7
Ilya Koshelev <qkoshelev@gmail.com>
2016-08-16 17:24:24 +0400

    + added trace error metrics, http metrics, custom metrics and trace-segments support for spm

--- kamon-spm/src/main/scala/kamon/spm/SPMMetric.scala
+++ kamon-spm/src/main/scala/kamon/spm/SPMMetric.scala
@@ -25,11 +25,9 @@ import kamon.util.MilliTimestamp
 
 import scala.util.Random
 
-case class SPMMetric(ts: MilliTimestamp, category: String, name: String, instrumentName: String, unitOfMeasurement: UnitOfMeasurement, snapshot: InstrumentSnapshot)
+case class SPMMetric(ts: MilliTimestamp, category: String, name: String, instrumentName: String, tags: Map[String, String], unitOfMeasurement: UnitOfMeasurement, snapshot: InstrumentSnapshot)
 
 object SPMMetric {
-
-  private val TraceDurationStoringThreshold = 30
 
   private def convert(unit: UnitOfMeasurement, value: Long): Long = unit match {
     case t: Time   ⇒ t.scale(Time.Milliseconds)(value).toLong
@@ -41,28 +39,75 @@ object SPMMetric {
     s"${metric.ts.millis}\t${metric.category}-${metric.instrumentName}\t${metric.ts.millis}\t${metric.name}"
   }
 
+  private def httpRequestPrefix(metric: SPMMetric): String = {
+    val code = metric.instrumentName.substring(metric.instrumentName.lastIndexOf('_') + 1)
+    val requestName = metric.instrumentName.substring(0, metric.instrumentName.lastIndexOf('_'))
+    if (code.matches("[4]\\d\\d")) {
+      s"${metric.ts.millis}\t${"http-4XX-response"}\t${metric.ts.millis}\t${requestName}"
+    } else if (code.matches("[5]\\d\\d")) {
+      s"${metric.ts.millis}\t${"http-5XX-response"}\t${metric.ts.millis}\t${requestName}"
+    } else {
+      s"${metric.ts.millis}\t${"http-calls"}\t${metric.ts.millis}\t${requestName}"
+    }
+  }
+
   def format(metric: SPMMetric): String = metric match {
-    case SPMMetric(_, _, _, _, unit, histo: Histogram#SnapshotType) ⇒ {
+    case SPMMetric(_, "http-server", _, name, _, unit, counter: Counter#SnapshotType) ⇒ {
+      s"${httpRequestPrefix(metric)}\t${counter.count}"
+    }
+    case SPMMetric(_, _, _, _, _, unit, histo: Histogram#SnapshotType) ⇒ {
       val min = convert(unit, histo.min)
       val max = convert(unit, histo.max)
       val sum = convert(unit, histo.sum)
-      s"${prefix(metric)}\t${min}\t${max}\t${sum}\t${histo.numberOfMeasurements}"
+      val p50 = convert(unit, histo.percentile(50D))
+      val p90 = convert(unit, histo.percentile(90D))
+      val p95 = convert(unit, histo.percentile(95D))
+      val p99 = convert(unit, histo.percentile(99D))
+      val p995 = convert(unit, histo.percentile(99.5D))
+      s"${prefix(metric)}\t${min}\t${max}\t${sum}\t${histo.numberOfMeasurements}\t${p50}\t${p90}\t${p95}\t${p99}\t${p995}"
     }
-    case SPMMetric(_, _, _, _, unit, counter: Counter#SnapshotType) ⇒ {
+    case SPMMetric(_, _, _, _, _, unit, counter: Counter#SnapshotType) ⇒ {
       s"${prefix(metric)}\t${counter.count}"
     }
   }
 
-  def traceFormat(metric: SPMMetric, token: String): Array[Byte] = metric match {
-    case SPMMetric(_, _, _, _, unit, histo: Histogram#SnapshotType) ⇒ {
+  def traceFormat(metric: SPMMetric, token: String, segments: List[SPMMetric]): Array[Byte] = metric match {
+    case SPMMetric(_, _, _, _, tags, unit, histo: Histogram#SnapshotType) ⇒ {
+      val tracingMetrics = segments.filter(segments ⇒ (segments.tags.get("trace") == metric.name))
       val event = new TTracingEvent()
       val thrift = new TPartialTransaction()
-      thrift.setCallId(Random.nextLong)
-      thrift.setParentCallId(0L)
-      thrift.setTraceId(Random.nextLong)
+      val rnd = new Random()
+      val callId = rnd.nextLong()
+      thrift.setParentCallId(0)
+      thrift.setCallId(callId)
+      thrift.setTraceId(rnd.nextLong())
+      val calls = new util.ArrayList[TCall]()
+      val tCall = new TCall()
+      tCall.setDuration(convert(unit, histo.max))
+      tCall.setStartTimestamp(metric.ts.millis)
+      tCall.setEndTimestamp(metric.ts.millis + convert(unit, histo.max))
+      tCall.setCallId(callId)
+      tCall.setParentCallId(0)
+      tCall.setSignature(metric.name)
+      calls.add(tCall)
+      segments.foreach((segment: SPMMetric) ⇒ {
+        segment match {
+          case SPMMetric(_, _, _, _, tags, unit, histo: Histogram#SnapshotType) ⇒ {
+            val tcall = new TCall()
+            tcall.setDuration(convert(unit, histo.max))
+            tcall.setStartTimestamp(segment.ts.millis)
+            tcall.setEndTimestamp(segment.ts.millis + convert(unit, histo.max))
+            tcall.setCallId(rnd.nextLong())
+            tcall.setParentCallId(callId)
+            tcall.setSignature(segment.name)
+            calls.add(tcall)
+          }
+          case others ⇒
+        }
+      })
       thrift.setRequest(metric.name)
       thrift.setStartTimestamp(metric.ts.millis)
-      thrift.setEndTimestamp(metric.ts.millis + histo.max)
+      thrift.setEndTimestamp(metric.ts.millis + convert(unit, histo.max))
       thrift.setDuration(convert(unit, histo.max))
       thrift.setToken(token)
       thrift.setFailed(false)
@@ -74,9 +119,48 @@ object SPMMetric {
       thrift.setTransactionSummary(ThriftUtils.binaryProtocolSerializer().serialize(summary))
       val endpoint = new TEndpoint()
       endpoint.setHostname(InetAddress.getLocalHost.getHostName)
-      endpoint.setAddress(InetAddress.getLocalHost.getAddress.toString)
+      endpoint.setAddress(InetAddress.getLocalHost.getHostAddress)
       thrift.setEndpoint(endpoint)
-      thrift.setCalls(new util.ArrayList[TCall]())
+      thrift.setCalls(calls)
+      thrift.setParameters(new util.HashMap[String, String]())
+      event.setPartialTransaction(thrift)
+      event.eventType = TTracingEventType.PARTIAL_TRANSACTION
+      ThriftUtils.binaryProtocolSerializer().serialize(event)
+    }
+    case SPMMetric(_, _, _, _, tags, unit, counter: Counter#SnapshotType) ⇒ {
+      val event = new TTracingEvent()
+      val thrift = new TPartialTransaction()
+      val calls = new util.ArrayList[TCall]()
+      val rnd = new Random()
+      val callId = rnd.nextLong()
+      val tCall = new TCall()
+      tCall.setDuration(0L)
+      tCall.setStartTimestamp(metric.ts.millis)
+      tCall.setEndTimestamp(metric.ts.millis)
+      tCall.setCallId(callId)
+      tCall.setParentCallId(0)
+      tCall.setSignature(metric.name)
+      calls.add(tCall)
+      thrift.setCallId(callId)
+      thrift.setParentCallId(0L)
+      thrift.setTraceId(Random.nextLong)
+      thrift.setRequest(metric.name)
+      thrift.setStartTimestamp(metric.ts.millis)
+      thrift.setEndTimestamp(metric.ts.millis)
+      thrift.setDuration(0L)
+      thrift.setToken(token)
+      thrift.setFailed(true)
+      thrift.setEntryPoint(true)
+      thrift.setAsynchronous(false)
+      thrift.setTransactionType(TTransactionType.WEB)
+      val summary = new TWebTransactionSummary()
+      summary.setRequest(metric.name)
+      thrift.setTransactionSummary(ThriftUtils.binaryProtocolSerializer().serialize(summary))
+      val endpoint = new TEndpoint()
+      endpoint.setHostname(InetAddress.getLocalHost.getHostName)
+      endpoint.setAddress(InetAddress.getLocalHost.getHostAddress)
+      thrift.setEndpoint(endpoint)
+      thrift.setCalls(calls)
       thrift.setParameters(new util.HashMap[String, String]())
       event.setPartialTransaction(thrift)
       event.eventType = TTracingEventType.PARTIAL_TRANSACTION
@@ -84,9 +168,15 @@ object SPMMetric {
     }
   }
 
-  def isTraceToStore(metric: SPMMetric): Boolean = metric match {
-    case SPMMetric(_, _, _, _, unit, histo: Histogram#SnapshotType) ⇒ {
-      convert(unit, histo.max) > TraceDurationStoringThreshold
+  def isTraceToStore(metric: SPMMetric, threshold: Int): Boolean = metric match {
+    case SPMMetric(_, _, _, _, _, unit, histo: Histogram#SnapshotType) ⇒ {
+      convert(unit, histo.max) > threshold
+    }
+    case SPMMetric(_, _, _, _, _, unit, counter: Counter#SnapshotType) ⇒ {
+      metric.instrumentName.equals("errors") && counter.count > 0
+    }
+    case others ⇒ {
+      false
     }
   }
 }
