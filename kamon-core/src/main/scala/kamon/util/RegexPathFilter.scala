fd7cfb420050ccf8afbb68a047c41068be9d85be
Daniel Bimschas <daniel@bimschas.com>
2016-11-05 04:10:03 +0100

    + core: support for regex path filters (#388)

---
+++ kamon-core/src/main/scala/kamon/util/RegexPathFilter.scala
@@ -0,0 +1,27 @@
+/*
+ * =========================================================================================
+ * Copyright © 2013-2015 the kamon project <http://kamon.io/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ * =========================================================================================
+ */
+
+package kamon.util
+
+case class RegexPathFilter(path: String) extends PathFilter {
+  private val pathRegex = path.r
+  override def accept(path: String): Boolean = {
+    path match {
+      case pathRegex(_*) ⇒ true
+      case _             ⇒ false
+    }
+  }
+}
