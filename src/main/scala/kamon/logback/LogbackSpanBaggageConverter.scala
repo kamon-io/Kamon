96c635017f4372f30b3079e9b52a4901494ee276
Tomo Ferega <tferega@gmail.com>
2017-06-28 20:23:16 +0200

    Add the token and baggage converters

---
+++ src/main/scala/kamon/logback/LogbackSpanBaggageConverter.scala
@@ -0,0 +1,33 @@
+/*
+ * =========================================================================================
+ * Copyright © 2013-2017 the kamon project <http://kamon.io/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ * =========================================================================================
+ */
+
+package kamon.logback
+
+import ch.qos.logback.classic.pattern.ClassicConverter
+import ch.qos.logback.classic.spi.ILoggingEvent
+import kamon.Kamon
+import kamon.trace.{SpanContext => KamonSpanContext}
+
+class LogbackSpanBaggageConverter extends ClassicConverter {
+  def convert(event: ILoggingEvent): String = {
+    val spanOpt = Option(Kamon.activeSpan)
+    val keyOpt = Option(getFirstOption)
+    (spanOpt, keyOpt) match {
+      case (Some(span), Some(key)) => Option(span.getBaggageItem(key)).getOrElse("undefined")
+      case _ => "undefined"
+    }
+  }
+}
