7c138f616d3aee678d91519b18c32ab327ba4857
Roman Filonenko <shkoder@users.noreply.github.com>
2018-03-17 16:52:15 +0100

    fix inevitable OutOfMemory   * do not allocate a client on each metric send in DatadogAPIReporter   * fix timestamp sent to datadog in the DatadogAPIReporter   * update deps   * quick brush up of code

--- src/main/scala/kamon/datadog/DatadogAgentReporter.scala
+++ src/main/scala/kamon/datadog/DatadogAgentReporter.scala
@@ -19,17 +19,16 @@ package kamon.datadog
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.channels.DatagramChannel
-import java.text.{DecimalFormat, DecimalFormatSymbols}
+import java.text.{ DecimalFormat, DecimalFormatSymbols }
 import java.util.Locale
 
 import com.typesafe.config.Config
-import kamon.{Kamon, MetricReporter}
+import kamon.{ Kamon, MetricReporter }
 import kamon.metric._
 import kamon.metric.MeasurementUnit
-import kamon.metric.MeasurementUnit.Dimension.{Information, Time}
-import kamon.metric.MeasurementUnit.{Dimension, information, time}
+import kamon.metric.MeasurementUnit.Dimension.{ Information, Time }
+import kamon.metric.MeasurementUnit.{ Dimension, information, time }
 import org.slf4j.LoggerFactory
-
 
 class DatadogAgentReporter extends MetricReporter {
   private val logger = LoggerFactory.getLogger(classOf[DatadogAgentReporter])
@@ -47,24 +46,24 @@ class DatadogAgentReporter extends MetricReporter {
 
   override def reconfigure(config: Config): Unit = {}
 
-
   override def reportPeriodSnapshot(snapshot: PeriodSnapshot): Unit = {
     val config = readConfiguration(Kamon.config())
     val clientChannel = DatagramChannel.open()
     val packetBuffer = new PacketBuffer(config.maxPacketSize, clientChannel, config.agentAddress)
     val serviceNameTag = "|#service:" + Kamon.environment.service
 
-
-    for(counter <- snapshot.metrics.counters) {
+    for (counter <- snapshot.metrics.counters) {
       packetBuffer.appendMeasurement(counter.name, formatMeasurement(encodeDatadogCounter(counter.value, counter.unit), serviceNameTag, counter.tags))
     }
 
-    for(gauge <- snapshot.metrics.gauges) {
+    for (gauge <- snapshot.metrics.gauges) {
       packetBuffer.appendMeasurement(gauge.name, formatMeasurement(encodeDatadogGauge(gauge.value, gauge.unit), serviceNameTag, gauge.tags))
     }
 
-    for(metric <- snapshot.metrics.histograms ++ snapshot.metrics.rangeSamplers;
-        bucket <- metric.distribution.bucketsIterator) {
+    for (
+      metric <- snapshot.metrics.histograms ++ snapshot.metrics.rangeSamplers;
+      bucket <- metric.distribution.bucketsIterator
+    ) {
 
       val bucketData = formatMeasurement(encodeDatadogHistogramBucket(bucket.value, bucket.frequency, metric.unit), serviceNameTag, metric.tags)
       packetBuffer.appendMeasurement(metric.name, bucketData)
@@ -75,16 +74,16 @@ class DatadogAgentReporter extends MetricReporter {
   }
 
   private def formatMeasurement(measurementData: String, baseTag: String, tags: Map[String, String]): String = {
-    val stringTags: String = if(tags.isEmpty) baseTag else baseTag + "," + (tags.map { case (k, v) ⇒ k + ":" + v } mkString ",")
+    val stringTags: String = if (tags.isEmpty) baseTag else baseTag + "," + (tags.map { case (k, v) ⇒ k + ":" + v } mkString ",")
 
     StringBuilder.newBuilder
-    .append(measurementData)
-    .append(stringTags)
-    .result()
+      .append(measurementData)
+      .append(stringTags)
+      .result()
   }
 
   private def encodeDatadogHistogramBucket(value: Long, frequency: Long, unit: MeasurementUnit): String = {
-    val metricType = if(unit.dimension == Dimension.Time) "ms" else "h"
+    val metricType = if (unit.dimension == Dimension.Time) "ms" else "h"
     val samplingRate: Double = 1D / frequency.toDouble
     valueFormat.format(scale(value, unit)) + "|" + metricType + (if (samplingRate != 1D) "|@" + samplingRateFormat.format(samplingRate) else "")
   }
@@ -96,11 +95,10 @@ class DatadogAgentReporter extends MetricReporter {
     valueFormat.format(scale(value, unit)) + "|g"
 
   private def scale(value: Long, unit: MeasurementUnit): Double = unit.dimension match {
-    case Time         if unit.magnitude != time.seconds       => MeasurementUnit.scale(value, unit, time.seconds)
-    case Information  if unit.magnitude != information.bytes  => MeasurementUnit.scale(value, unit, information.bytes)
-    case _ => value
+    case Time if unit.magnitude != time.seconds.magnitude             => MeasurementUnit.scale(value, unit, time.seconds)
+    case Information if unit.magnitude != information.bytes.magnitude => MeasurementUnit.scale(value, unit, information.bytes)
+    case _                                                            => value.toDouble
   }
-
 
   private def readConfiguration(config: Config): Configuration = {
     val datadogConfig = config.getConfig("kamon.datadog")
@@ -109,13 +107,11 @@ class DatadogAgentReporter extends MetricReporter {
       agentAddress = new InetSocketAddress(datadogConfig.getString("agent.hostname"), datadogConfig.getInt("agent.port")),
       maxPacketSize = datadogConfig.getBytes("agent.max-packet-size"),
       timeUnit = readTimeUnit(datadogConfig.getString("time-unit")),
-      informationUnit = readInformationUnit(datadogConfig.getString("information-unit"))
-    )
+      informationUnit = readInformationUnit(datadogConfig.getString("information-unit")))
   }
 
   private case class Configuration(agentAddress: InetSocketAddress, maxPacketSize: Long, timeUnit: MeasurementUnit,
-    informationUnit: MeasurementUnit)
-
+                                   informationUnit: MeasurementUnit)
 
   private class PacketBuffer(maxPacketSizeInBytes: Long, channel: DatagramChannel, remote: InetSocketAddress) {
     val metricSeparator = "\n"
@@ -127,7 +123,7 @@ class DatadogAgentReporter extends MetricReporter {
       val data = key + measurementSeparator + measurementData
 
       if (fitsOnBuffer(metricSeparator + data)) {
-        val mSeparator = if (buffer.length > 0) metricSeparator else ""
+        val mSeparator = if (buffer.nonEmpty) metricSeparator else ""
         buffer.append(mSeparator).append(data)
       } else {
         flushToUDP(buffer.toString())
@@ -138,7 +134,7 @@ class DatadogAgentReporter extends MetricReporter {
 
     def fitsOnBuffer(data: String): Boolean = (buffer.length + data.length) <= maxPacketSizeInBytes
 
-    private def flushToUDP(data: String): Unit =  {
+    private def flushToUDP(data: String): Unit = {
       println(data)
       channel.send(ByteBuffer.wrap(data.getBytes), remote)
     }
--- src/main/scala/kamon/datadog/package.scala
+++ src/main/scala/kamon/datadog/package.scala
@@ -1,24 +1,24 @@
 package kamon
 
 import kamon.metric.MeasurementUnit
-import kamon.metric.MeasurementUnit.{time}
+import kamon.metric.MeasurementUnit.{ time, information }
 
 package object datadog {
 
   def readTimeUnit(unit: String): MeasurementUnit = unit match {
-    case "s"    => time.seconds
-    case "ms"   => time.milliseconds
-    case "µs"   => time.microseconds
-    case "ns"   => time.nanoseconds
-    case other  => sys.error(s"Invalid time unit setting [$other], the possible values are [s, ms, µs, ns]")
+    case "s"   => time.seconds
+    case "ms"  => time.milliseconds
+    case "µs"  => time.microseconds
+    case "ns"  => time.nanoseconds
+    case other => sys.error(s"Invalid time unit setting [$other], the possible values are [s, ms, µs, ns]")
   }
 
   def readInformationUnit(unit: String): MeasurementUnit = unit match {
-    case "b"    => time.seconds
-    case "kb"   => time.milliseconds
-    case "mb"   => time.microseconds
-    case "gb"   => time.nanoseconds
-    case other  => sys.error(s"Invalid time unit setting [$other], the possible values are [s, ms, µs, ns]")
+    case "b"   => information.bytes
+    case "kb"  => information.kilobytes
+    case "mb"  => information.megabytes
+    case "gb"  => information.gigabytes
+    case other => sys.error(s"Invalid time unit setting [$other], the possible values are [b, kb, mb, gb]")
   }
 
 }
