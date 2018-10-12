60d60f5b0cdb79264861219bf869cfced6be3744
Ivan Topolnjak <ivantopo@gmail.com>
2018-10-12 14:46:36 +0200

    bring back the slow tests

--- kamon-core-tests/src/test/scala/kamon/KamonLifecycleSpec.scala
+++ kamon-core-tests/src/test/scala/kamon/KamonLifecycleSpec.scala
@@ -12,7 +12,7 @@ import org.scalatest.time.SpanSugar._
 
 class KamonLifecycleSpec extends WordSpec with Matchers with Eventually{
 
-  "the Kamon lifecycle" ignore {
+  "the Kamon lifecycle" should {
     "keep the JVM running if reporters are running" in {
       val process = Runtime.getRuntime.exec(createProcessCommand("kamon.KamonWithRunningReporter"))
       Thread.sleep(5000)
