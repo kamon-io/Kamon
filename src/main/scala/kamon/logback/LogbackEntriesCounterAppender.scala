c2e463a32d97e2706e97a238a63121e3168a8215
Octavio Bokel <octavio.bokel@tnt-digital.com>
2019-03-06 15:06:54 +0100

    Counter on object

--- src/main/scala/kamon/logback/LogbackEntriesCounterAppender.scala
+++ src/main/scala/kamon/logback/LogbackEntriesCounterAppender.scala
@@ -7,14 +7,14 @@ import LogbackEntriesCounterAppender._
 
 class LogbackEntriesCounterAppender extends UnsynchronizedAppenderBase[ILoggingEvent] {
 
-  private val counter = Kamon.counter(CounterName)
-
   protected def append(event: ILoggingEvent): Unit =
     counter.refine(LevelTagName → event.getLevel.levelStr).increment()
+
 }
 
 object LogbackEntriesCounterAppender{
   private val appenderConfig = Kamon.config().getConfig("kamon.logback.entries-counter")
   private[logback] val CounterName = appenderConfig.getString("metric-name")
   private[logback] val LevelTagName = appenderConfig.getString("level-tag-name")
+  private[logback] val counter = Kamon.counter(CounterName)
 }
