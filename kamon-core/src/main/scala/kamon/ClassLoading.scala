6b797388cbf2fe3b4ea962c9a7202b468d147422
Ivan Topolnjak <ivantopo@gmail.com>
2019-06-03 17:30:27 +0200

    move ClassLoading to a dedicated companion object

--- kamon-core/src/main/scala/kamon/ClassLoading.scala
+++ kamon-core/src/main/scala/kamon/ClassLoading.scala
@@ -9,7 +9,7 @@ import scala.reflect.ClassTag
   * Exposes APIs for dynamically creating instances and holds the ClassLoader instance to be used by Kamon when looking
   * up resources and classes.
   */
-object ClassLoading {
+sealed trait ClassLoading {
 
   @volatile private var _dynamicAccessClassLoader = this.getClass.getClassLoader
   @volatile private var _dynamicAccess = new DynamicAccess(_dynamicAccessClassLoader)
@@ -31,31 +31,33 @@ object ClassLoading {
   /**
     * Tries to create an instance of a class with the provided fully-qualified class name and its no-arg constructor.
     */
-  private[kamon] def createInstance[T: ClassTag](fqcn: String): T =
+  def createInstance[T: ClassTag](fqcn: String): T =
     _dynamicAccess.createInstanceFor(fqcn, immutable.Seq.empty)
 
   /**
     * Tries to create an instance of a class with the provided fully-qualified class name and the provided constructor
     * arguments.
     */
-  private[kamon] def createInstance[T: ClassTag](fqcn: String, args: immutable.Seq[(Class[_], AnyRef)]): T =
+  def createInstance[T: ClassTag](fqcn: String, args: immutable.Seq[(Class[_], AnyRef)]): T =
     _dynamicAccess.createInstanceFor(fqcn, args)
 
   /**
     * Tries to create an instance of the provided Class with its no-arg constructor.
     */
-  private[kamon] def createInstance[T: ClassTag](clazz: Class[_]): T =
+  def createInstance[T: ClassTag](clazz: Class[_]): T =
     _dynamicAccess.createInstanceFor(clazz, immutable.Seq.empty)
 
   /**
     * Tries to create an instance of with the provided Class and constructor arguments.
     */
-  private[kamon] def createInstance[T: ClassTag](clazz: Class[_], args: immutable.Seq[(Class[_], AnyRef)]): T =
+  def createInstance[T: ClassTag](clazz: Class[_], args: immutable.Seq[(Class[_], AnyRef)]): T =
     _dynamicAccess.createInstanceFor(clazz, args)
 
   /**
     * Tries load a class with the provided fully-qualified class name.
     */
-  private[kamon] def resolveClass[T: ClassTag](fqcn: String): Class[_ <: T] =
+  def resolveClass[T: ClassTag](fqcn: String): Class[_ <: T] =
     _dynamicAccess.getClassFor(fqcn)
 }
+
+object ClassLoading extends ClassLoading
