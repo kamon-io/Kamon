5ec29a6e36eb2048b8b3170a48fc5dd397131387
João Ferreira <jtjeferreira@gmail.com>
2019-07-24 12:03:42 +0100

    reintroduce increment/decrement API on Gauge (#596)
    * reintroduce increment/decrement on Gauge
    * protect from negatives values, but use an AtomicLong
    * longBitsToDouble
    * scala 2.11 support
    * add tests for the new increment/decrement gauge APIs
    * use openjdk8
    * fix conversion

--- kamon-core/src/main/scala/kamon/metric/Gauge.scala
+++ kamon-core/src/main/scala/kamon/metric/Gauge.scala
@@ -16,15 +16,38 @@
 
 package kamon.metric
 
+import java.util.concurrent.atomic.AtomicLong
+import java.util.function.LongUnaryOperator
+
 import kamon.metric.Metric.{BaseMetric, BaseMetricAutoUpdate}
 import kamon.tag.TagSet
-
+import java.lang.Double.{longBitsToDouble,doubleToLongBits}
 
 /**
   * Instrument that tracks the latest observed value of a given measure.
   */
 trait Gauge extends Instrument[Gauge, Metric.Settings.ForValueInstrument] {
 
+  /**
+   * Increments the current value by one.
+   */
+  def increment(): Gauge
+
+  /**
+   * Increments the current value the provided number of times.
+   */
+  def increment(times: Double): Gauge
+
+  /**
+   * Decrements the current value by one.
+   */
+  def decrement(): Gauge
+
+  /**
+   * Decrements the current value the provided number of times.
+   */
+  def decrement(times: Double): Gauge
+  
   /**
     * Sets the current value of the gauge to the provided value.
     */
@@ -40,17 +63,42 @@ object Gauge {
   class Volatile(val metric: BaseMetric[Gauge, Metric.Settings.ForValueInstrument, Double], val tags: TagSet) extends Gauge
       with Instrument.Snapshotting[Double] with BaseMetricAutoUpdate[Gauge, Metric.Settings.ForValueInstrument, Double] {
 
-    @volatile private var _currentValue = 0D
+    //https://stackoverflow.com/questions/5505460/java-is-there-no-atomicfloat-or-atomicdouble
+    private val _currentValue = new AtomicLong(0)
+
+    override def increment(): Gauge = increment(1)
+
+    override def increment(times: Double): Gauge = {
+      _currentValue.updateAndGet(new LongUnaryOperator {
+        override def applyAsLong(v: Long): Long = {
+          val newValue: Double = longBitsToDouble(v) + times
+          if(newValue < 0) v else doubleToLongBits(newValue)
+        }
+      })
+      this
+    }
+
+    override def decrement(): Gauge = decrement(1)
+
+    override def decrement(times: Double): Gauge = {
+      _currentValue.updateAndGet(new LongUnaryOperator {
+        override def applyAsLong(v: Long): Long = {
+          val newValue: Double = longBitsToDouble(v) - times
+          if(newValue < 0) v else doubleToLongBits(newValue)
+        }
+      })
+      this
+    }
 
     override def update(newValue: Double): Gauge = {
       if(newValue >= 0D)
-        _currentValue = newValue
+        _currentValue.set(doubleToLongBits(newValue))
 
       this
     }
 
     override def snapshot(resetState: Boolean): Double =
-      _currentValue
+      longBitsToDouble(_currentValue.get())
 
     override def baseMetric: BaseMetric[Gauge, Metric.Settings.ForValueInstrument, Double] =
       metric
