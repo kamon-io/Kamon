25cc4ffdce9e87194a9592d9c65f6803a277544b
slasch <slavaschmidt@gmx.de>
2019-08-29 10:13:35 +0200

    + core: provide span propagation codec for "uber-trace-id", default jaeger format

--- core/kamon-core/src/main/scala/kamon/trace/SpanPropagation.scala
+++ core/kamon-core/src/main/scala/kamon/trace/SpanPropagation.scala
@@ -25,11 +25,20 @@ import kamon.context.generated.binary.span.{Span => ColferSpan}
 import kamon.context.{Context, _}
 import kamon.trace.Trace.SamplingDecision
 
+import scala.util.Try
+
 
 /**
   * Propagation mechanisms for Kamon's Span data to and from HTTP and Binary mediums.
   */
 object SpanPropagation {
+
+  object Util {
+    def urlEncode(s: String): String = URLEncoder.encode(s, "UTF-8")
+    def urlDecode(s: String): String = URLDecoder.decode(s, "UTF-8")
+  }
+
+  import Util._
 
   /**
     * Reads and Writes a Span instance using the B3 propagation format. The specification and semantics of the B3
@@ -88,8 +97,6 @@ object SpanPropagation {
       case SamplingDecision.Unknown     => None
     }
 
-    private def urlEncode(s: String): String = URLEncoder.encode(s, "UTF-8")
-    private def urlDecode(s: String): String = URLDecoder.decode(s, "UTF-8")
   }
 
   object B3 {
@@ -170,8 +177,6 @@ object SpanPropagation {
       case SamplingDecision.Unknown     => None
     }
 
-    private def urlEncode(s: String): String = URLEncoder.encode(s, "UTF-8")
-    private def urlDecode(s: String): String = URLDecoder.decode(s, "UTF-8")
   }
 
   object B3Single {
@@ -193,6 +198,80 @@ object SpanPropagation {
       new B3Single()
   }
 
+
+  /**
+   * Reads and Writes a Span instance using the jaeger single-header propagation format.
+   * The specification and semantics can be found here:
+   *   https://www.jaegertracing.io/docs/1.7/client-libraries/#propagation-format
+   *
+   * The description somewhat ambiguous, a lots of implementation details are second-guessed from existing clients
+   */
+  object Uber {
+    def apply(): Uber = new Uber()
+    val HeaderName = "uber-trace-id"
+    val Separator = ":"
+    val Default = "0"
+    val DebugFlag = "d"
+  }
+
+  class Uber extends Propagation.EntryReader[HeaderReader] with Propagation.EntryWriter[HeaderWriter] {
+    import Uber._
+
+    override def write(context: Context, writer: HttpPropagation.HeaderWriter): Unit = {
+      val span = context.get(Span.Key)
+
+      if (span != Span.Empty) {
+        val parentContext = if (span.parentId != Identifier.Empty) span.parentId.string else Default
+        val sampling = encodeSamplingDecision(span.trace.samplingDecision)
+        val debug: Byte = 0
+        val flags = (sampling + (debug << 1)).toHexString
+        val headerValue = Seq(span.trace.id.string, span.id.string, parentContext, flags).mkString(Separator)
+
+        writer.write(HeaderName, urlEncode(headerValue))
+      }
+
+    }
+
+    override def read(reader: HttpPropagation.HeaderReader, context: Context): Context = {
+      val identifierScheme = Kamon.identifierScheme
+      val header = reader.read(HeaderName)
+      val headerParts = header.toList.flatMap(_.split(':'))
+      val parts = headerParts ++ List.fill(4)("") // all parts are mandatory, but we want to be resilient
+
+      val List(traceID, spanID, parentContext, flags) = parts.take(4)
+      val trace = stringToId(identifierScheme, traceID)
+      val span = stringToId(identifierScheme, spanID)
+
+      if (trace != Identifier.Empty && span != Identifier.Empty) {
+        val parent = stringToId(identifierScheme, parentContext)
+        val samplingDecision = decodeSamplingDecision(flags)
+        context.withEntry(Span.Key, Span.Remote(span, parent, Trace(trace, samplingDecision)))
+      } else {
+        context
+      }
+    }
+
+    private def stringToId(identifierScheme: Identifier.Scheme, s: String) = {
+      val str = if (s == null || s.isEmpty) None else Option(s)
+      val id = str.map(urlDecode).map(identifierScheme.traceIdFactory.from)
+      id.getOrElse(Identifier.Empty)
+    }
+
+    private def lowestBit(s: String) = Try(Integer.parseInt(s, 16) % 2).toOption
+
+    private def decodeSamplingDecision(flags: String) =
+      if (flags.equalsIgnoreCase(DebugFlag)) SamplingDecision.Sample
+      else if (lowestBit(flags).contains(1)) SamplingDecision.Sample
+      else if (lowestBit(flags).contains(0)) SamplingDecision.DoNotSample
+      else SamplingDecision.Unknown
+
+    private def encodeSamplingDecision(samplingDecision: SamplingDecision): Byte = samplingDecision match {
+      case SamplingDecision.Sample      => 1
+      case SamplingDecision.DoNotSample => 0
+      case SamplingDecision.Unknown     => 0 // the sampling decision is mandatory in this format
+    }
+
+  }
 
   /**
     * Defines a bare bones binary context propagation that uses Colfer [1] as the serialization library. The Schema
