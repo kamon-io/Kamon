6faad2bbb7b3e295ccfeef22fedfc862eadfc82c
SimunKaracic <simunkaracic@gmail.com>
2020-10-23 14:36:51 +0200

    Add sbt-headers to generate licences

--- project/Build.scala
+++ project/Build.scala
@@ -113,6 +113,7 @@ object BaseProject extends AutoPlugin {
     licenses += (("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0"))),
     resolvers += Resolver.bintrayRepo("kamon-io", "releases"),
     resolvers += Resolver.mavenLocal,
+    headerLicense := Some(HeaderLicense.ALv2("2013-2020","The Kamon Project <https://kamon.io>")),
     Keys.commands += Command.command("testUntilFailed") { state: State =>
       "test" :: "testUntilFailed" :: state
     }
