e6fc7657c8f7f6f3906ed7f61d70e8243b6d532c
SimunKaracic <simunkaracic@gmail.com>
2020-11-23 21:19:47 +0100

    Update oshi core version

--- project/Build.scala
+++ project/Build.scala
@@ -35,7 +35,7 @@ object BaseProject extends AutoPlugin {
     val hdrHistogram      = "org.hdrhistogram"      %  "HdrHistogram"    % "2.1.10"
     val okHttp            = "com.squareup.okhttp3"  %  "okhttp"          % "3.14.7"
     val okHttpMockServer  = "com.squareup.okhttp3"  %  "mockwebserver"   % "3.10.0"
-    val oshiCore          = "com.github.oshi"       %  "oshi-core"       % "5.3.4"
+    val oshiCore          = "com.github.oshi"       %  "oshi-core"       % "5.3.6"
 
 
     val kanelaAgentVersion = settingKey[String]("Kanela Agent version")
