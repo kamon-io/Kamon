16b64bc912ed4999c41f8786181286be3c1a6669
SimunKaracic <simunkaracic@gmail.com>
2021-01-13 19:05:08 +0100

    Refactor gzip implementation
    It was not closing connections correctly on gzip requests

--- reporters/kamon-prometheus/src/main/scala/kamon/prometheus/embeddedhttp/SunEmbeddedHttpServer.scala
+++ reporters/kamon-prometheus/src/main/scala/kamon/prometheus/embeddedhttp/SunEmbeddedHttpServer.scala
@@ -21,10 +21,13 @@ import com.typesafe.config.Config
 import kamon.prometheus.ScrapeSource
 import kamon.prometheus.embeddedhttp.SunEmbeddedHttpServer.shouldUseCompression
 
+import java.io.OutputStream
 import java.net.{InetAddress, InetSocketAddress}
 import java.nio.charset.StandardCharsets
+import java.util
 import java.util.zip.GZIPOutputStream
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 class SunEmbeddedHttpServer(hostname: String, port: Int, scrapeSource: ScrapeSource, config: Config) extends EmbeddedHttpServer(hostname, port, scrapeSource, config) {
   private val server = {
@@ -35,18 +38,19 @@ class SunEmbeddedHttpServer(hostname: String, port: Int, scrapeSource: ScrapeSou
         if (httpExchange.getRequestURI.getPath == "/metrics") {
           val data = scrapeSource.scrapeData()
           val bytes = data.getBytes(StandardCharsets.UTF_8)
-          val os = httpExchange.getResponseBody
+          var os: OutputStream = null
           try {
             if (shouldUseCompression(httpExchange)) {
-            val gzip = new GZIPOutputStream(os)
-            httpExchange.sendResponseHeaders(200, 0)
-            httpExchange.getResponseHeaders.set("Content-Encoding", "gzip")
-            gzip.write(bytes)
-          } else {
-            httpExchange.sendResponseHeaders(200, bytes.length)
-            os.write(bytes)
-          }
-          } finally os.close()
+              httpExchange.getResponseHeaders.set("Content-Encoding", "gzip")
+              httpExchange.sendResponseHeaders(200, 0)
+              os = new GZIPOutputStream(httpExchange.getResponseBody)
+              os.write(bytes)
+            } else {
+              os = httpExchange.getResponseBody
+              httpExchange.sendResponseHeaders(200, bytes.length)
+              os.write(bytes)
+            }
+          } finally Option(os).map(_.close())
         } else httpExchange.sendResponseHeaders(404, -1)
       }
     }
@@ -61,16 +65,15 @@ class SunEmbeddedHttpServer(hostname: String, port: Int, scrapeSource: ScrapeSou
 
 object SunEmbeddedHttpServer {
   def shouldUseCompression(httpExchange: HttpExchange): Boolean = {
-    val encodingHeaders = httpExchange.getRequestHeaders.asScala.get("Accept-Encoding")
-    encodingHeaders match {
-      case Some(headerList) =>
-        val trimmedEncodings = for {
-          encodingHeader <- headerList.asScala
-          encodings = encodingHeader.split(",")
-          encoding <- encodings
-        } yield encoding.trim().toLowerCase()
-        trimmedEncodings.contains("gzip")
-      case None => false
-    }
+    httpExchange.getRequestHeaders
+      .asScala.get("Accept-Encoding")
+      .map(extractEncodings)
+      .exists(_.contains("gzip"))
+  }
+
+  private def extractEncodings(headerList: util.List[String]): mutable.Buffer[String] = {
+    headerList.asScala
+      .flatMap(_.split(","))
+      .map(_.trim().toLowerCase())
   }
 }
