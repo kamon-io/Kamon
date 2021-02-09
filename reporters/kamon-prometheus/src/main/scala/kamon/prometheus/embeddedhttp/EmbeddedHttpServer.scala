68f23191aeb7e2a9328f1ea93262aaa53808e119
SimunKaracic <simunkaracic@gmail.com>
2021-02-09 10:12:45 +0100

    Remove extension to EmbeddedHttpServer

--- reporters/kamon-prometheus/src/main/scala/kamon/prometheus/embeddedhttp/EmbeddedHttpServer.scala
+++ reporters/kamon-prometheus/src/main/scala/kamon/prometheus/embeddedhttp/EmbeddedHttpServer.scala
@@ -19,6 +19,6 @@ package kamon.prometheus.embeddedhttp
 import com.typesafe.config.Config
 import kamon.prometheus.ScrapeSource
 
-abstract class EmbeddedHttpServer(hostname: String, port: Int, path: String, scrapeSource: ScrapeSource, config: Config) {
+abstract class EmbeddedHttpServer(hostname: String, port: Int, scrapeSource: ScrapeSource, config: Config) {
   def stop(): Unit
 }
