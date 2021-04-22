9b165edb39c17d15cb615e5b46a7d0bd112e8eb9
Ivan Topolnjak <ivantopo@kamon.io>
2021-04-22 10:44:20 +0200

    cosmetic changes, file relocation and license headers

--- instrumentation/kamon-tapir/src/test/scala/kamon/instrumentation/tapir/TestRoutes.scala
+++ instrumentation/kamon-tapir/src/test/scala/kamon/instrumentation/tapir/TestRoutes.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013-2021 The Kamon Project <https://kamon.io>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package kamon.instrumentation.tapir
 
 import akka.http.scaladsl.server.Directives._
@@ -16,9 +32,9 @@ object TestRoutes {
 
   // hello/{}/with/{}/mixed/{}/types
   private val nested = endpoint.get
-    .in("nested").in(path[String])
+    .in("nested").in(path[String]("integer_param"))
     .in("with").in(path[Int])
-    .in("mixed").in(path[Boolean])
+    .in("mixed").in(path[Boolean]("other_param"))
     .in("types").out(plainBody[String])
   private val nestedRoute = AkkaHttpServerInterpreter.toRoute(nested)(x => Future.successful(Right(x.toString())))
 
