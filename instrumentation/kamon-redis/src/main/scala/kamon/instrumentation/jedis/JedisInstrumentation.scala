f5247bfda955fc6a2b5ca49148e53aa4220463d7
SimunKaracic <simunkaracic@gmail.com>
2021-05-28 09:39:44 +0200

    Remove element matcher, replace with pattern match

--- instrumentation/kamon-redis/src/main/scala/kamon/instrumentation/jedis/JedisInstrumentation.scala
+++ instrumentation/kamon-redis/src/main/scala/kamon/instrumentation/jedis/JedisInstrumentation.scala
@@ -8,27 +8,29 @@ import redis.clients.jedis.commands.ProtocolCommand
 
 class JedisInstrumentation extends InstrumentationBuilder {
   onType("redis.clients.jedis.Protocol")
-    .advise(method("sendCommand").and(withArgument(1, classOf[ProtocolCommand])), classOf[SendCommandAdvice])
+    .advise(method("sendCommand"), classOf[SendCommandAdvice])
 }
 
 class SendCommandAdvice
-
 object SendCommandAdvice {
   @Advice.OnMethodEnter()
-  def enter(@Advice.Argument(1) command: ProtocolCommand) = {
-    val spanName = s"redis.command.${command}"
-    val span = Kamon.clientSpanBuilder(spanName, "redis.client.jedis")
-      .start()
-
-    span
+  def enter(@Advice.Argument(1) command: Any) = {
+    command match {
+      case command: ProtocolCommand =>
+        val spanName = s"redis.command.${command}"
+        Some(Kamon.clientSpanBuilder(spanName, "redis.client.jedis")
+          .start())
+      case _ => None
+    }
   }
 
   @Advice.OnMethodExit(onThrowable = classOf[Throwable])
-  def exit(@Advice.Enter span: Span,
+  def exit(@Advice.Enter span: Option[Span],
            @Advice.Thrown t: Throwable) = {
+
     if (t != null) {
-      span.fail(t)
+      span.map(_.fail(t))
     }
-    span.finish()
+    span.map(_.finish())
   }
 }
