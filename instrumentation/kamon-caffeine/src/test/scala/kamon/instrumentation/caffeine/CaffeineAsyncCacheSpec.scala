41d6c1cfb49bd3af49ed3ffb951c2c17960377f3
SimunKaracic <simunkaracic@gmail.com>
2021-07-05 11:53:37 +0200

    Test that async caches dont create spans

---
+++ instrumentation/kamon-caffeine/src/test/scala/kamon/instrumentation/caffeine/CaffeineAsyncCacheSpec.scala
@@ -0,0 +1,51 @@
+package kamon.instrumentation.caffeine
+
+import com.github.benmanes.caffeine.cache.{AsyncCache, Caffeine}
+import kamon.testkit.TestSpanReporter
+import org.scalatest.concurrent.Eventually.eventually
+import org.scalatest.concurrent.Waiters.timeout
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
+
+import java.util.concurrent.CompletableFuture
+import scala.collection.JavaConverters._
+import scala.concurrent.duration.DurationInt
+
+class CaffeineAsyncCacheSpec
+  extends WordSpec
+    with Matchers
+    with BeforeAndAfterAll
+    with TestSpanReporter {
+  "Caffeine instrumentation for async caches" should {
+    val cache: AsyncCache[String, String] = Caffeine.newBuilder()
+      .buildAsync[String, String]()
+
+    "not create a span when using put" in {
+      cache.put("a", CompletableFuture.completedFuture("key"))
+      eventually(timeout(2.seconds)) {
+        testSpanReporter().spans() shouldBe empty
+      }
+    }
+
+    "not create a span when using get" in {
+      cache.get("a", (a: String) => "value")
+      eventually(timeout(2.seconds)) {
+        testSpanReporter().spans() shouldBe empty
+      }
+    }
+
+    "not create a span when using getAll" in {
+      val valueMap = Map("b" -> "value", "c" -> "value").asJava
+      cache.getAll(List("a", "b", "c").asJava, _ => valueMap)
+      eventually(timeout(2.seconds)) {
+        testSpanReporter().spans() shouldBe empty
+      }
+    }
+
+    "not create a span when using getIfPresent" in {
+      cache.getIfPresent("not_exists")
+      eventually(timeout(2.seconds)) {
+        testSpanReporter().spans() shouldBe empty
+      }
+    }
+  }
+}
