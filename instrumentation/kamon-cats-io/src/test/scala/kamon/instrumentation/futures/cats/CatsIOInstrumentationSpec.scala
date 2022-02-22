c9b0686c13db12a077b89ec0e58d8d8c052eff63
cmcmteixeira <cmcmteixeira@gmail.com>
2022-02-22 18:27:50 +0000

    Fixed tests.

--- instrumentation/kamon-cats-io/src/test/scala/kamon/instrumentation/futures/cats/CatsIOInstrumentationSpec.scala
+++ instrumentation/kamon-cats-io/src/test/scala/kamon/instrumentation/futures/cats/CatsIOInstrumentationSpec.scala
@@ -10,12 +10,17 @@ import org.scalatest.concurrent.{Eventually, PatienceConfiguration, ScalaFutures
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-import java.util.concurrent.Executors
+import java.util.concurrent.{Executors, ScheduledExecutorService}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.global
 import scala.concurrent.duration._
 import cats.implicits._
-import kamon.trace.Span
+import kamon.tag.TagSet
+import kamon.trace.Identifier.Scheme
+import kamon.trace.Tracer.LocalTailSamplerSettings
+import kamon.trace.{ConstantSampler, Identifier, Sampler, Span, Trace}
+
+import java.time.{Clock, Instant}
 
 class CatsIoInstrumentationSpec extends AnyWordSpec with Matchers with ScalaFutures with PatienceConfiguration
     with OptionValues with Eventually {
@@ -23,7 +28,6 @@ class CatsIoInstrumentationSpec extends AnyWordSpec with Matchers with ScalaFutu
   // NOTE: We have this test just to ensure that the Context propagation is working, but starting with Kamon 2.0 there
   //       is no need to have explicit Runnable/Callable instrumentation because the instrumentation brought by the
   //       kamon-executors module should take care of all non-JDK Runnable/Callable implementations.
-
   "an cats.effect IO created when instrumentation is active" should {
     "capture the active span available when created" which {
       "must be available across asynchronous boundaries" in {
@@ -60,7 +64,12 @@ class CatsIoInstrumentationSpec extends AnyWordSpec with Matchers with ScalaFutu
       }
 
       "must allow complex Span topologies to be created" in {
-        val context = Context.of("key", "value")
+        val parentSpan = Span.Remote(
+          Scheme.Single.spanIdFactory.generate(),
+          Identifier.Empty,
+          Trace.create(Scheme.Single.traceIdFactory.generate(), Trace.SamplingDecision.Sample)
+        )
+        val context = Context.of(Span.Key, parentSpan)
         /**
           * test
           *   - nestedLevel0
@@ -70,19 +79,22 @@ class CatsIoInstrumentationSpec extends AnyWordSpec with Matchers with ScalaFutu
           */
         val test = for {
           span <- IO.delay(Kamon.currentSpan())
-          nestedLevel0 <- meteredWithSpanCapture("level1-A")(IO.sleep(1.seconds))
-          nestedUpToLevel2 <- meteredWithSpanCapture("level1-B")(meteredWithSpanCapture("level2-B")(IO.sleep(1.seconds)))
-          fiftyInParallel <- (0 to 50).toList.parTraverse(i => meteredWithSpanCapture(s"operation$i")(IO.sleep(1.seconds)))
+          nestedLevel0 <- meteredWithSpanCapture("level1-A")(IO.sleep(100.millis))
+          nestedUpToLevel2 <- meteredWithSpanCapture("level1-B")(meteredWithSpanCapture("level2-B")(IO.sleep(100.millis)))
+          fiftyInParallel <- (0 to 49).toList.parTraverse(i => meteredWithSpanCapture(s"operation$i")(IO.sleep(100.millis)))
+          afterEverything <- IO.delay(Kamon.currentSpan())
         } yield {
           span.id.string should not be empty
           span.id.string shouldBe nestedLevel0._1.parentId.string
           span.id.string shouldBe nestedUpToLevel2._1.parentId.string
           nestedUpToLevel2._1.id.string shouldBe nestedUpToLevel2._2._1.parentId.string
           fiftyInParallel.map(_._1.parentId.string).toSet shouldBe Set(span.id.string)
+          fiftyInParallel.map(_._1.id.string).toSet should have size 50
+          afterEverything.id.string shouldBe span.id.string
         }
 
         val runtime = IORuntime.global
-        (IO.delay(Kamon.storeContext(context)) *> meteredWithSpanCapture("test")(test)).unsafeRunSync()(runtime)
+        (IO.delay(Kamon.init()) *> IO.delay(Kamon.storeContext(context)) *> test).unsafeRunSync()(runtime)
       }
     }
   }
@@ -94,12 +106,18 @@ class CatsIoInstrumentationSpec extends AnyWordSpec with Matchers with ScalaFutu
     Resource
       .make{
         for {
-          ctx <- IO.delay(Kamon.currentContext())
+          initialCtx <- IO.delay(Kamon.currentContext())
           parentSpan <- IO.delay(Kamon.currentSpan())
-          span <- IO.delay(Kamon.spanBuilder(operation).context(ctx).asChildOf(parentSpan).start())
-          _ <- IO.delay(Kamon.storeContext(ctx.withEntry(Context.key("span", span), span)))
-        } yield span
-      }(span => IO.delay(span.finish()))
+          newSpan     <- IO.delay(Kamon.spanBuilder(operation).context(initialCtx).asChildOf(parentSpan).start())
+          _           <- IO.delay(Kamon.storeContext(initialCtx.withEntry(Span.Key, newSpan)))
+        } yield (initialCtx, newSpan)
+      }{
+        case (initialCtx, span) =>
+          for {
+            _ <- IO.delay(span.finish())
+              _ <- IO.delay(Kamon.storeContext(initialCtx))
+          } yield ()
+      }
       .use(_ => (IO.delay(Kamon.currentSpan()), io).parBisequence)
   }
 
