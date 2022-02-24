eafe66d9f62441b12611b99198f938ec408530a5
Ivan Topolnjak <ivantopo@kamon.io>
2022-02-24 09:44:01 +0100

    apply gRPC tags and take a sampling decision on Akka gRPC server requests, fixes #1118

--- instrumentation/kamon-akka-http/src/main/scala-2.13/kamon/instrumentation/akka/http/AkkaHttpServerInstrumentation.scala
+++ instrumentation/kamon-akka-http/src/main/scala-2.13/kamon/instrumentation/akka/http/AkkaHttpServerInstrumentation.scala
@@ -25,6 +25,7 @@ import akka.NotUsed
 import akka.http.scaladsl.server.RouteResult.Rejected
 import akka.stream.scaladsl.Flow
 import kamon.context.Context
+import kanela.agent.libs.net.bytebuddy.asm.Advice
 import kanela.agent.libs.net.bytebuddy.matcher.ElementMatchers.isPublic
 
 import scala.collection.immutable
@@ -84,6 +85,19 @@ class AkkaHttpServerInstrumentation extends InstrumentationBuilder {
 
   onType("akka.http.scaladsl.Http2Ext")
     .advise(method("bindAndHandleAsync") and isPublic(), classOf[Http2ExtBindAndHandleAdvice])
+
+
+  /**
+    * Support for Akka gRPC servers.
+    *
+    * gRPC requests get their spans started by the ServerFlowWrapper like any other requests, but they never go
+    * through any instrumentation that gives a good operation name to the Span and forces taking a sampling decision.
+    * This instrumentation gives a proper name and tags to the span when it matches one of the exposed services,
+    * otherwise the span remains unchanged. Assumes no actual implementation of `akka.grpc.internal.TelemetrySpi` is
+    * configured.
+    */
+  onType("akka.grpc.internal.NoOpTelemetry$")
+    .advise(method("onRequest"), AkkaGRPCServerSampling)
 }
 
 trait HasMatchingContext {
@@ -318,5 +332,20 @@ object Http2BlueprintInterceptor {
       case _ =>
         zuper.call()
     }
+  }
+}
+
+object AkkaGRPCServerSampling {
+
+  @Advice.OnMethodEnter()
+  def enter(@Advice.Argument(0) serviceName: String, @Advice.Argument(1) method: String): Unit = {
+    val fullSpanName = serviceName + "/" + method
+    Kamon.currentSpan()
+      .name(fullSpanName)
+      .tagMetrics("component", "akka.grpc.server")
+      .tagMetrics("rpc.system", "grpc")
+      .tagMetrics("rpc.service", serviceName)
+      .tagMetrics("rpc.method", method)
+      .takeSamplingDecision()
   }
 }
