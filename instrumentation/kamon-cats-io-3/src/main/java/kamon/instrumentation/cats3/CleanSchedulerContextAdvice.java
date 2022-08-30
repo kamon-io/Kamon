8ebff487ef161da3212efc0f6572cacb6b77e88f
Ivan Topolnjak <ivantopo@kamon.io>
2022-08-30 10:16:24 +0200

    update the cats effect 3 instrumentation

---
+++ instrumentation/kamon-cats-io-3/src/main/java/kamon/instrumentation/cats3/CleanSchedulerContextAdvice.java
@@ -0,0 +1,38 @@
+package kamon.instrumentation.cats3;
+
+import kamon.Kamon;
+import kamon.context.Context;
+import kamon.context.Storage;
+import kanela.agent.libs.net.bytebuddy.asm.Advice;
+
+public class CleanSchedulerContextAdvice {
+
+  @Advice.OnMethodEnter
+  public static void enter(@Advice.Argument(value = 1, readOnly = false) Runnable runnable) {
+    runnable = new ContextCleaningWrapper(runnable, Kamon.currentContext());
+  }
+
+
+  public static class ContextCleaningWrapper implements Runnable {
+    private final Runnable runnable;
+    private final Context context;
+
+    public ContextCleaningWrapper(Runnable runnable, Context context) {
+      this.runnable = runnable;
+      this.context = context;
+    }
+
+    @Override
+    public void run() {
+      final Storage.Scope scope = Kamon.storeContext(context);
+
+      try {
+        runnable.run();
+      } catch (Throwable e) {
+        scope.close();
+      }
+
+      Kamon.storeContext(Context.Empty());
+    }
+  }
+}
