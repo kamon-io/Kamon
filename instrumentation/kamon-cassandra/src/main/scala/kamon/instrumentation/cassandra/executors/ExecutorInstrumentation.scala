50054ca54e65155cedf6f85ef92469ea153622a5
Hugh Simpson <hsimpson@rzsoftware.com>
2023-12-01 12:25:28 +0000

    some cassandra support

--- instrumentation/kamon-cassandra/src/main/scala/kamon/instrumentation/cassandra/executors/ExecutorInstrumentation.scala
+++ instrumentation/kamon-cassandra/src/main/scala/kamon/instrumentation/cassandra/executors/ExecutorInstrumentation.scala
@@ -17,12 +17,13 @@
 package kamon.instrumentation.cassandra.executors
 
 import java.util.concurrent.{Callable, ExecutorService, ScheduledExecutorService}
-
 import kamon.instrumentation.cassandra.CassandraInstrumentation
 import kamon.instrumentation.executor.ExecutorInstrumentation
 import kamon.tag.TagSet
 import kanela.agent.api.instrumentation.InstrumentationBuilder
 import kanela.agent.libs.net.bytebuddy.implementation.bind.annotation.SuperCall
+
+import scala.annotation.static
 
 class DriverExecutorInstrumentation extends InstrumentationBuilder {
 
@@ -30,11 +31,11 @@ class DriverExecutorInstrumentation extends InstrumentationBuilder {
     * Wraps all executors created by the Cassandra driver with the Kamon executors instrumentation.
     */
   onType("com.datastax.driver.core.ThreadingOptions")
-    .intercept(method("createExecutor"), CreateExecutorAdvice)
-    .intercept(method("createBlockingExecutor"), CreateBlockingTasksExecutorAdvice)
-    .intercept(method("createReaperExecutor"), CreateReaperExecutorAdvice)
-    .intercept(method("createScheduledTasksExecutor"), CreateScheduledTasksExecutorAdvice)
-    .intercept(method("createReconnectionExecutor"), CreateReconnectionExecutorAdvice)
+    .intercept(method("createExecutor"), classOf[CreateExecutorAdvice])
+    .intercept(method("createBlockingExecutor"), classOf[CreateBlockingTasksExecutorAdvice])
+    .intercept(method("createReaperExecutor"), classOf[CreateReaperExecutorAdvice])
+    .intercept(method("createScheduledTasksExecutor"), classOf[CreateScheduledTasksExecutorAdvice])
+    .intercept(method("createReconnectionExecutor"), classOf[CreateReconnectionExecutorAdvice])
 }
 
 sealed trait ExecutorMetrics {
@@ -51,32 +52,37 @@ sealed trait ExecutorMetrics {
     ExecutorInstrumentation.instrumentScheduledExecutor(callable.call(), metricName(name), componentTags)
 }
 
+class CreateExecutorAdvice
 object CreateExecutorAdvice extends ExecutorMetrics {
-  def onExecutorCreated(@SuperCall callable: Callable[ExecutorService]): ExecutorService =
+  @static def onExecutorCreated(@SuperCall callable: Callable[ExecutorService]): ExecutorService =
     instrument(callable, "executor")
 }
 
+class CreateBlockingTasksExecutorAdvice
 object CreateBlockingTasksExecutorAdvice extends ExecutorMetrics {
-  def onExecutorCreated(@SuperCall callable: Callable[ExecutorService]): ExecutorService =
+  @static def onExecutorCreated(@SuperCall callable: Callable[ExecutorService]): ExecutorService =
     instrument(callable, "blocking")
 }
 
+class CreateReaperExecutorAdvice
 object CreateReaperExecutorAdvice extends ExecutorMetrics {
-  def onExecutorCreated(
+  @static def onExecutorCreated(
       @SuperCall callable: Callable[ScheduledExecutorService]
   ): ScheduledExecutorService =
     instrumentScheduled(callable, "reaper")
 }
 
+class CreateScheduledTasksExecutorAdvice
 object CreateScheduledTasksExecutorAdvice extends ExecutorMetrics {
-  def onExecutorCreated(
+  @static def onExecutorCreated(
       @SuperCall callable: Callable[ScheduledExecutorService]
   ): ScheduledExecutorService =
     instrumentScheduled(callable, "scheduled-tasks")
 }
 
+class CreateReconnectionExecutorAdvice
 object CreateReconnectionExecutorAdvice extends ExecutorMetrics {
-  def onExecutorCreated(
+  @static def onExecutorCreated(
       @SuperCall callable: Callable[ScheduledExecutorService]
   ): ScheduledExecutorService =
     instrumentScheduled(callable, "reconnection")
