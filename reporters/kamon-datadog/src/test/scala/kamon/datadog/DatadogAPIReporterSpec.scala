b8dea69da77b56be830afd4e4cade6a81230a1f5
André Cardoso <thyandrecardoso@gmail.com>
2024-10-11 08:24:50 +0100

    = kamon-datadog: Add p99 metric (#1360)

--- reporters/kamon-datadog/src/test/scala/kamon/datadog/DatadogAPIReporterSpec.scala
+++ reporters/kamon-datadog/src/test/scala/kamon/datadog/DatadogAPIReporterSpec.scala
@@ -65,6 +65,43 @@ class DatadogAPIReporterSpec extends AbstractHttpReporter with Matchers with Rec
       server.getRequestCount shouldEqual 4
     }
 
+    val examplePeriodWithDistributions: PeriodSnapshot = {
+      val distributionExample = new Distribution {
+        override def dynamicRange: DynamicRange = ???
+        override def min: Long = 0
+        override def max: Long = 10
+        override def sum: Long = 100
+        override def count: Long = 5
+        override def percentile(rank: Double): Distribution.Percentile = new Percentile {
+          override def rank: Double = 0
+          override def value: Long = 0
+          override def countAtRank: Long = 0
+        }
+        override def percentiles: Seq[Distribution.Percentile] = ???
+        override def percentilesIterator: Iterator[Distribution.Percentile] = ???
+        override def buckets: Seq[Distribution.Bucket] = ???
+        override def bucketsIterator: Iterator[Distribution.Bucket] = ???
+      }
+      PeriodSnapshot.apply(
+        now.minusMillis(1000),
+        now,
+        Nil,
+        Nil,
+        Nil,
+        MetricSnapshot.ofDistributions(
+          "test.timer",
+          "test",
+          Metric.Settings.ForDistributionInstrument(
+            MeasurementUnit.none,
+            java.time.Duration.ZERO,
+            DynamicRange.Default
+          ),
+          Instrument.Snapshot.apply(TagSet.Empty, distributionExample) :: Nil
+        ) :: Nil,
+        Nil
+      )
+    }
+
     "sends metrics - compressed" in {
       val baseUrl = mockResponse("/test", new MockResponse().setStatus("HTTP/1.1 200 OK"))
       applyConfig("kamon.datadog.api.api-url = \"" + baseUrl + "\"")
@@ -103,50 +140,14 @@ class DatadogAPIReporterSpec extends AbstractHttpReporter with Matchers with Rec
 
     }
 
-    "send timer metrics" in {
+    "send timer metrics with the p95 percentile by default" in {
       val baseUrl = mockResponse("/test", new MockResponse().setStatus("HTTP/1.1 200 OK"))
       applyConfig("kamon.datadog.api.api-url = \"" + baseUrl + "\"")
       applyConfig("kamon.datadog.api.api-key = \"dummy\"")
       applyConfig("kamon.datadog.api.compression = false")
       reporter.reconfigure(Kamon.config())
 
-      val distribution = new Distribution {
-        override def dynamicRange: DynamicRange = ???
-        override def min: Long = 0
-        override def max: Long = 10
-        override def sum: Long = 100
-        override def count: Long = 5
-        override def percentile(rank: Double): Distribution.Percentile = new Percentile {
-          override def rank: Double = 0
-          override def value: Long = 0
-          override def countAtRank: Long = 0
-        }
-        override def percentiles: Seq[Distribution.Percentile] = ???
-        override def percentilesIterator: Iterator[Distribution.Percentile] = ???
-        override def buckets: Seq[Distribution.Bucket] = ???
-        override def bucketsIterator: Iterator[Distribution.Bucket] = ???
-      }
-
-      reporter.reportPeriodSnapshot(
-        PeriodSnapshot.apply(
-          now.minusMillis(1000),
-          now,
-          Nil,
-          Nil,
-          Nil,
-          MetricSnapshot.ofDistributions(
-            "test.timer",
-            "test",
-            Metric.Settings.ForDistributionInstrument(
-              MeasurementUnit.none,
-              java.time.Duration.ZERO,
-              DynamicRange.Default
-            ),
-            Instrument.Snapshot.apply(TagSet.Empty, distribution) :: Nil
-          ) :: Nil,
-          Nil
-        )
-      )
+      reporter.reportPeriodSnapshot(examplePeriodWithDistributions)
       val request = server.takeRequest()
       request.getRequestUrl.toString shouldEqual baseUrl + "?api_key=dummy"
       request.getMethod shouldEqual "POST"
@@ -157,6 +158,55 @@ class DatadogAPIReporterSpec extends AbstractHttpReporter with Matchers with Rec
             |{"metric":"test.timer.count","interval":1,"points":[[1523394,5]],"type":"count","host":"test","tags":["env:staging","service:kamon-application"]},
             |{"metric":"test.timer.median","interval":1,"points":[[1523394,0]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]},
             |{"metric":"test.timer.95percentile","interval":1,"points":[[1523394,0]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]},
+            |{"metric":"test.timer.max","interval":1,"points":[[1523394,10]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]},
+            |{"metric":"test.timer.min","interval":1,"points":[[1523394,0]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]}]}""".stripMargin
+        )
+    }
+
+    "send timer metrics allowing configuration of percentiles to submit" in {
+      val baseUrl = mockResponse("/test", new MockResponse().setStatus("HTTP/1.1 200 OK"))
+      applyConfig("kamon.datadog.api.api-url = \"" + baseUrl + "\"")
+      applyConfig("kamon.datadog.api.api-key = \"dummy\"")
+      applyConfig("kamon.datadog.api.compression = false")
+      applyConfig("kamon.datadog.percentiles = [95.0, 99, 94.5]")
+      reporter.reconfigure(Kamon.config())
+
+      reporter.reportPeriodSnapshot(examplePeriodWithDistributions)
+      val request = server.takeRequest()
+      request.getRequestUrl.toString shouldEqual baseUrl + "?api_key=dummy"
+      request.getMethod shouldEqual "POST"
+      Json.parse(request.getBody.readUtf8()) shouldEqual Json
+        .parse(
+          """{"series":[
+            |{"metric":"test.timer.avg","interval":1,"points":[[1523394,20]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]},
+            |{"metric":"test.timer.count","interval":1,"points":[[1523394,5]],"type":"count","host":"test","tags":["env:staging","service:kamon-application"]},
+            |{"metric":"test.timer.median","interval":1,"points":[[1523394,0]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]},
+            |{"metric":"test.timer.95percentile","interval":1,"points":[[1523394,0]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]},
+            |{"metric":"test.timer.99percentile","interval":1,"points":[[1523394,0]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]},
+            |{"metric":"test.timer.94.5percentile","interval":1,"points":[[1523394,0]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]},
+            |{"metric":"test.timer.max","interval":1,"points":[[1523394,10]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]},
+            |{"metric":"test.timer.min","interval":1,"points":[[1523394,0]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]}]}""".stripMargin
+        )
+    }
+
+    "send timer metrics without percentiles" in {
+      val baseUrl = mockResponse("/test", new MockResponse().setStatus("HTTP/1.1 200 OK"))
+      applyConfig("kamon.datadog.api.api-url = \"" + baseUrl + "\"")
+      applyConfig("kamon.datadog.api.api-key = \"dummy\"")
+      applyConfig("kamon.datadog.api.compression = false")
+      applyConfig("kamon.datadog.percentiles = []")
+      reporter.reconfigure(Kamon.config())
+
+      reporter.reportPeriodSnapshot(examplePeriodWithDistributions)
+      val request = server.takeRequest()
+      request.getRequestUrl.toString shouldEqual baseUrl + "?api_key=dummy"
+      request.getMethod shouldEqual "POST"
+      Json.parse(request.getBody.readUtf8()) shouldEqual Json
+        .parse(
+          """{"series":[
+            |{"metric":"test.timer.avg","interval":1,"points":[[1523394,20]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]},
+            |{"metric":"test.timer.count","interval":1,"points":[[1523394,5]],"type":"count","host":"test","tags":["env:staging","service:kamon-application"]},
+            |{"metric":"test.timer.median","interval":1,"points":[[1523394,0]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]},
             |{"metric":"test.timer.max","interval":1,"points":[[1523394,10]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]},
             |{"metric":"test.timer.min","interval":1,"points":[[1523394,0]],"type":"gauge","host":"test","tags":["env:staging","service:kamon-application"]}]}""".stripMargin
         )
